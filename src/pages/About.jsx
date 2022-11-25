f5fc19d7f9b293be6fe541ee7a13e5a8d0d5edce
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-25 00:02:40 -0600

    updating errors

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1 className='text-6xl mb-4'>Github Finder</h1>
+        <p className='mb-4 text-2xl font-light'>    
+            A React app to search Github profiles and see profile deitals!
+        </p>
+    </div>
+  )
+}
+
+export default About
